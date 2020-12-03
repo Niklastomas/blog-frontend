13f633d910c56db37fdeff66c40577f4da7a2e9e
Niklas Tomas <niklas.thomas@hotmail.com>
2020-12-03 11:53:31 +0100

    added registerView, update userSlice

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import LoginView from '../views/loginView/LoginView';
+import RegisterView from '../views/registerView/RegisterView';
 
 function App() {
   return (
     <div className='app'>
-      <LoginView />
+      {/* <LoginView /> */}
+      <RegisterView />
     </div>
   );
 }
